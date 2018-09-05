bf059295f300b3c20bca744bcb662a0c44da77a0
Okello Emmanuel <emmanuel.alvino.okello06@gmail.com>
2018-09-05 11:36:56 +0300

    Add endpoints for Assets

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,162 @@ const Branding = mongoose.model('Branding');
 /*const Admin = mongoose.model('Admin');*/
 /*const UserInfo = mongoose.model('UserInfo');*/
 
+
+
+
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+
+
+
+
+
+
+//Routes and Endpoints for Assets
+
+//Get all assets
+router.get('', (req, res, next) => {
+  Assets.find((err, data) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+
+
+
+//Get asset by id
+router.get('/:id', (req, res, next) => {
+  if (req.params && req.params.id) {
+    Assets.findById(req.params.id, (err, data) => {
+      if (data) {
+        res.json(data);
+      } else {
+        res.send(err)
+      }
+    })
+  } else {
+    res.send("ID not found");
+  }
+});
+
+
+
+
+//Add asset
+router.post('', (req, res, next) => {
+  let addAsset = new Assets({
+
+    tagNumber: req.body.tagNumber,
+    serialNumber: req.body.serialNumber,
+    assetClass: req.body.assetClass,
+    itemName: req.body.itemName,
+    manufacturerSeller: req.body.manufacturerSeller,
+    color: req.body.color,
+    numberOfItems: req.body.numberOfItems,
+    itemDescription: req.body.itemDescription,
+    owner: req.body.owner,
+    category: req.body.category,
+    dateOfPurchase: req.body.dateOfPurchase,
+    purchasePrice: req.body.purchasePrice
+  })
+  addAsset.save((err, data) => {
+    if (err) {
+      res.send("Failed to add asset");
+    } else {
+      res.send("Asset successfully added");
+    }
+  });
+});
+
+
+
+
+
+//Update asset
+router.put('/:id', (req, res, next) => {
+  if (!(req.params && req.params.id)) {
+    res.status(404).send("Invalid ID");
+  } else {
+    let edit = {
+      tagNumber: req.body.tagNumber,
+      serialNumber: req.body.serialNumber,
+      assetClass: req.body.assetClass,
+      itemName: req.body.itemName,
+      manufacturerSeller: req.body.manufacturerSeller,
+      color: req.body.color,
+      numberOfItems: req.body.numberOfItems,
+      itemDescription: req.body.itemDescription,
+      owner: req.body.owner,
+      category: req.body.category,
+      dateOfPurchase: req.body.dateOfPurchase,
+      purchasePrice: req.body.purchasePrice
+    };
+    
+  }
+})
+
+
+
+
+
+
+//Routes and Endpoints for Branding
+
+
+
+
+
+
+
+
+
+
+//Routes and Endpoints for Consumption
+
+
+
+
+
+
+
+
+//Routes and Endpoints for Events
+
+
+
+
+
+
+
+
+//Routes and Endpoints for Utilities
+
+
+
+
+
+
+
+//Routes and Endpoints for Payments
+
+
+
+
+
+
+
+
+//Routes and Endpoints for Services
+
+
+
 
 
 
