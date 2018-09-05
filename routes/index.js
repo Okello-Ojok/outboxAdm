9be4dc8450f15055865046add3fe6b54ecbe6ef5
Okello Emmanuel <emmanuel.alvino.okello06@gmail.com>
2018-09-05 11:29:05 +0300

    Define and require models in the routes file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+
+const Utilities = mongoose.model('Utilities');
+const Consumption = mongoose.model('Consumption');
+const Events = mongoose.model('Events');
+const Assets = mongoose.model('Assets');
+const Services = mongoose.model('Services');
+const Payments = mongoose.model('Payments');
+const Branding = mongoose.model('Branding');
+/*const UserReg = mongoose.model('UserReg');*/
+/*const Admin = mongoose.model('Admin');*/
+/*const UserInfo = mongoose.model('UserInfo');*/
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+
+
+
 
 module.exports = router;
