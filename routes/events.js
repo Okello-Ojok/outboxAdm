a28948de25f0a3eaa9ac9af02c1550a8dd25b8e0
emukungu <enmukungu@gmail.com>
2018-09-07 11:30:08 +0300

    add a new event to the database and also return all existing events.

--- routes/events.js
+++ routes/events.js
@@ -5,11 +5,34 @@ var mongoose = require('mongoose');
 
 const Events = mongoose.model('Events');
 
+// register new event
+router.post('/add',(req, res) =>{
+  // read form the form
+  var eventName = req.body.eventName
+  var eventDate = req.body.eventDate
+  var eventTime = req.body.eventTime
+  var speaker = req.body.speaker
 
+  // create a document instance from the model
+  var events = new Events({
+    eventname:eventName,
+    eventDate:eventDate,
+    eventTime:eventTime,
+    speakerId: speaker
+  })
+  // save the instance
+  events.save((err, docs)=>{
+  if (err) throw err
+  res.json({"results":docs})
+  })
+});
 
-
-
-
+router.get('/', function(req, res, next){
+  var result = Events.find({},function(err, docs){
+    // if (err) throw err
+    res.send(docs)
+  })
+});
 
 
 
